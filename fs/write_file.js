2ff0f2ea62d736c846b218a0f44089c380280fe7
汐潼 <1922906183@qq.com>
2020-08-29 14:43:00 +0800

    添加文件

--- fs/write_file.js
+++ fs/write_file.js
@@ -1,3 +1,25 @@
-// const fs = require("fs");
+const fs = require("fs");
 
-// fs.writeFile(file, data[, options], callback);
+function writeFile(file, data, options = { encoding: 'utf8', mode: 0o666, flag: 'w' }) {
+    return new Promise((resolve, reject) => {
+        // 文件写入
+        fs.writeFile(file, data, options, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+(async function() {
+    let num = await writeFile('write.txt', 520); // 覆盖文字
+    console.log(num);
+
+    let str = await writeFile('write.txt', 'abc', { flag: 'a' }); // 添加文字
+    // write==>w  read==>r  append==>a
+    console.log(str);
+
+    console.log('程序执行完毕');
+})();
